109b0712abb0fc9db07f8c80f872af98ff7f2c01
nataliadiotto <diottonatalia@gmail.com>
2022-04-23 18:47:02 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				System.out.println();
@@ -54,6 +54,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
